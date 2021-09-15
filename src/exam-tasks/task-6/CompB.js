e009ea5214f37a3c40f74bd3a1693b2644660d96
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-15 12:18:32 +0300

    created counter

--- src/exam-tasks/task-6/CompB.js
+++ src/exam-tasks/task-6/CompB.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { ButtonsContext } from './Task6';
 
 function CompB() {
+  // hooks
+  // - state
+  // -- global
+  const clicksContext = useContext(ButtonsContext);
+  const { dispatch } = clicksContext;
+
+  // -- local
+  const [buttonClicks, setButtonClicks] = useState(0);
+
+  // custom functions
+  const clickHandlerPlus = () => {
+    // changing global state
+    dispatch({ type: 'INCREMENT' });
+    // changing local state
+    setButtonClicks(buttonClicks + 1);
+  };
+
+  const clickHandlerMinus = () => {
+    // changing global state
+    dispatch({ type: 'DECREMENT' });
+    // changing local state
+    setButtonClicks(buttonClicks - 1);
+  };
+
   return (
     <div>
       <h4>CompB</h4>
+      <p>{buttonClicks}</p>
+      <button onClick={clickHandlerMinus}>-</button>
+      <button onClick={clickHandlerPlus}>+</button>
     </div>
   );
 }
