2f657dfebda9a251d68280138a1999aac3ba3dfa
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-15 12:33:09 +0300

    created content for task 3

--- src/exam-tasks/task-3/Task3.js
+++ src/exam-tasks/task-3/Task3.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { FaTwitter } from 'react-icons/fa';
+import './Task3.css';
 
 function Task3() {
   return (
     <div>
       <h3>Task 3</h3>
+      <div>
+        <div>
+          <FaTwitter className='twitter-icon' />
+        </div>
+        <h2>Happening Now</h2>
+        <h4>Join Twitter today.</h4>
+        <button>Sign up</button>
+        <button>Log in</button>
+      </div>
     </div>
   );
 }
