40d5dd0b6d742f8234c1611f830c77c62c78e354
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-15 12:17:06 +0300

    created about us page

---
+++ src/exam-tasks/task-1/Pages/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <main>
+      <section>
+        <h1>About Us</h1>
+        <button>Grįžti atgal</button>
+      </section>
+    </main>
+  );
+};
+
+export default AboutUs;
