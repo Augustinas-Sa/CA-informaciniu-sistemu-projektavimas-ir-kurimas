f0b9339d35a34b845e1123714e48aec7a4322ce6
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-15 12:17:23 +0300

    created contacts page

---
+++ src/exam-tasks/task-1/Pages/Contacts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <main>
+      <section>
+        <h1>Contacts</h1>
+        <button>Grįžti atgal</button>
+      </section>
+    </main>
+  );
+};
+
+export default Contacts;
