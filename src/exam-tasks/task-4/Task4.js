bb6ad400ba1a7f38fe201e555b95ad2ae2015ef2
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-15 12:17:44 +0300

    created table

--- src/exam-tasks/task-4/Task4.js
+++ src/exam-tasks/task-4/Task4.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Task4() {
+  // Hooks
+  // - state
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Side effects
+  useEffect(() => {
+    axios.get('hptts://jsonplaceholder.typicode.com/users').then((response) => {
+      setUsers(response.data);
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <div>
       <h3>Task 4</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Adress</th>
+            <th>Phone</th>
+            <th>Company (name)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.address.street}, {user.address.city}
+                </td>
+                <td>{user.phone}</td>
+                <td>{user.company.name}</td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
@@ -18,7 +60,7 @@ Atkurkite lentelę, kurioje bus randama ši informacija apie vartotoją:
 - phone;
 - company (name).
 
-Informaciją imkite iš čia: https://jsonplaceholder.typicode.com/users
+Informaciją imkite iš čia: hptts://jsonplaceholder.typicode.com/users
 
 Pastaba: naudokite useState, useEffect.
 */
